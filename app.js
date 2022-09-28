1d68d0d243c4cc735804610a74aa7eafb8241720
Wardah Arshad <54935867+wardaharshad@users.noreply.github.com>
2022-09-29 03:10:31 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+// alert('Hello')
+
+
+// var name = "Mustafa" ;
+
+// document.write(name)
+
+
+// var name1 = "Ali" ;
+
+// console.log(name1)
+
+// var name$ = "Talha" ;
+
+// document.write(name$)
+
+
+// var first_name = "Rehan" ;
+// var last_name = "Aarib"
+
+// document.write(first_name)
+
+// document.write(last_name)
+
+// var NAME = "Talha" ;
+
+// document.write(NAME)
+
+
+
+// var HELLO = "HEllo World" ;
+
+// document.write(HELLO)
+
+// var str = "I am here to learn JavaScript"
+
+// console.log(str)
+
+// var num = 70 ;
+
+// document.write(num)
+
+// var num1 = 90 ;
+
+
+// console.log( typeof(num1) )
+
+
+// var str = "Hello World" ;
+
+// console.log( typeof(str) )
+
+// var data =  "Hello World" ;
+
+
+var num = 10 ; 
+console.log(num)
+var num2 = num ; 
+console.log(num2)
+var num3 = num + num2 ;
+console.log(num3)
+ //        10  +  10 +  40  + 20
+var num4 = num + num2 + 40 + num3 ;
+console.log(num4-20);
+//          10 +  10 + 100 + 80 + 20 + 10
+var num5 =  num + num2 + 100 + num4 + num3 +10;
+
+console.log(num5-40)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
